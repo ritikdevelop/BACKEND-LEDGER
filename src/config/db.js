61a33715b7bf958c77373972a195a1b056a10a60
ritikdevelop <hrithikrana216@gmail.com>
2026-02-12 15:26:14 +0530

    Created a db.js file and connected a database to the backend

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+
+function connectDB() {
+    mongoose.connect(process.env.MONGODB_URI)
+        .then(() => {
+            console.log('Connected to MongoDB');
+        })
+        .catch((err) => {
+            console.error('Error connecting to MongoDB:', err);
+            process.exit(1);
+        });
+}
+
+module.exports = connectDB;
