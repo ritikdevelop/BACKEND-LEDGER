28ea08baab0a28fa60c0616ff64f0988ff98a201
ritikdevelop <hrithikrana216@gmail.com>
2026-02-18 11:50:13 +0530

    `Updated import statements and added newline at end of file`

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
-
-
+const mongoose = require("mongoose");
 
 function connectDB() {
-    mongoose.connect(process.env.MONGODB_URI)
-        .then(() => {
-            console.log('Connected to MongoDB');
-        })
-        .catch((err) => {
-            console.error('Error connecting to MongoDB:', err);
-            process.exit(1);
-        });
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+      process.exit(1);
+    });
 }
 
 module.exports = connectDB;
